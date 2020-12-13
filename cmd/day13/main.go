83f0930dca6de77ca1ffe86ade8132961d88400f
Acksell <axelen@kth.se>
2020-12-13 22:23:22 +0100

    Removed unused code. Some comments

--- cmd/day13/main.go
+++ cmd/day13/main.go
@@ -10,17 +10,6 @@ import (
 
 const inputFilePath = "../../inputs/buses.txt"
 
-func IsLCM(ints []int, k int) bool {
-	isLCM := true
-	for _, i := range ints {
-		if i%k != 0 {
-			isLCM = false
-			break
-		}
-	}
-	return isLCM
-}
-
 func main() {
 	input := make(util.StringSlice, 0)
 	util.ReadLines(inputFilePath, &input)
@@ -28,8 +17,8 @@ func main() {
 	arrival, _ := strconv.Atoi(input[0])
 	busIDs := strings.Split(input[1], ",")
 	buses := make([]int, 0)
-	busMap := make(map[int]int)
-	for i, b := range busIDs {
+	busMap := make(map[int]int) // store the positions needed for part 2.
+	for i, b := range busIDs {  // convert busID strings to ints and append to buses slice.
 		if b == "x" {
 			continue
 		} else {
@@ -44,8 +33,8 @@ func main() {
 		delays = append(delays, b-(arrival%b))
 	}
 	mindelay, i := util.Min(delays)
-	fmt.Println(mindelay * buses[i])
-	// part 2, use prime
+	fmt.Println(mindelay * buses[i]) // part 1 answer.
+	// part 2
 	time := 1
 	n := 1
 	for _, b := range buses {
@@ -55,5 +44,5 @@ func main() {
 		}
 		n *= b
 	}
-	fmt.Println(time)
+	fmt.Println(time) // part 2 answer.
 }
